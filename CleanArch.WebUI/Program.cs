fd8a5cd696ed97f798727f4616632cf7cd62508e
lucasbonafe1 <l2005bonafe@gmail.com>
2025-08-01 22:26:19 -0300

    feat: defaultConnection pro banco

--- CleanArch.WebUI/Program.cs
+++ CleanArch.WebUI/Program.cs
@@ -1,7 +1,19 @@
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Context;
+using CleanArch.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+                        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+                            b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
