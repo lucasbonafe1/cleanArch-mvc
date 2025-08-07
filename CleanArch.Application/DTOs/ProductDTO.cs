0971edd8033f77005672d2e3f152983fb49bbf10
lucasbonafe1 <l2005bonafe@gmail.com>
2025-08-07 14:59:16 -0300

    fix: corrige motivo de não estar conseguindo setar campo de image, e melhora estrutura da página

--- CleanArch.Application/DTOs/ProductDTO.cs
+++ CleanArch.Application/DTOs/ProductDTO.cs
@@ -6,8 +6,8 @@ public class ProductDTO
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public decimal Price { get; set; }
-    public int Stock { get; private set; }
-    public string? Image { get; private set; }
+    public int Stock { get; set; }
+    public string? Image { get; set; }
     public int CategoryId { get; set; }
     public string? NameCategory { get; set; }
 }
